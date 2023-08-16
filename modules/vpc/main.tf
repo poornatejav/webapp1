111e16319611d2128ac14c372cbc56dd9f77b4b7
poornatejav <poornateja340@gmail.com>
2023-08-16 15:29:48 -0500

    first

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -18,45 +18,78 @@ resource "aws_internet_gateway" "internet_gateway" {
 
 data "aws_availability_zones" "available_zones" {}
 
+# resource "aws_subnet" "public_subnet" {
+#   vpc_id    = aws_vpc.vpc.id
+#   cidr_block              = var.public_cidr 
+#   availability_zone       = data.aws_availability_zones.available_zones.names[0]
+#   map_public_ip_on_launch = true
+
+#   tags      = {
+#     Name    = "public subnet"
+#   }
+# }
+
+
+# resource "aws_route_table" "public_route_table" {
+#   vpc_id    = aws_vpc.vpc.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.internet_gateway.id
+#   }
+
+#   tags       = {
+#     Name     = "public route table"
+#   }
+# }
+
+# resource "aws_route_table_association" "public_subnet_route_table_association" {
+#   subnet_id           = aws_subnet.public_subnet.id
+#   route_table_id      = aws_route_table.public_route_table.id
+# }
+
+
+# resource "aws_subnet" "private_subnet" {
+#   vpc_id    = aws_vpc.vpc.id
+#   cidr_block               = var.private_cidr
+#   availability_zone        = data.aws_availability_zones.available_zones.names[1]
+#   map_public_ip_on_launch  = false
+
+#   tags      = {
+#     Name    = "private subnet"
+#   }
+# }
+
+
 resource "aws_subnet" "public_subnet" {
-  vpc_id    = aws_vpc.vpc.id
-  cidr_block              = var.public_cidr 
-  availability_zone       = data.aws_availability_zones.available_zones.names[0]
+  count      = length(var.public_cidr)
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.public_cidr[count.index]
+  availability_zone = data.aws_availability_zones.available_zones.names[count.index]
   map_public_ip_on_launch = true
 
-  tags      = {
-    Name    = "public subnet"
+  tags = {
+    Name = "public_subnet_${count.index}"
   }
 }
 
-
 resource "aws_route_table" "public_route_table" {
-  vpc_id    = aws_vpc.vpc.id
+  count = length(var.public_cidr)
+  vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
-  tags       = {
-    Name     = "public route table"
+  tags = {
+    Name = "public_route_table_${count.index}"
   }
 }
 
 resource "aws_route_table_association" "public_subnet_route_table_association" {
-  subnet_id           = aws_subnet.public_subnet.id
-  route_table_id      = aws_route_table.public_route_table.id
-}
-
-
-resource "aws_subnet" "private_subnet" {
-  vpc_id    = aws_vpc.vpc.id
-  cidr_block               = var.private_cidr
-  availability_zone        = data.aws_availability_zones.available_zones.names[1]
-  map_public_ip_on_launch  = false
-
-  tags      = {
-    Name    = "private subnet"
-  }
+  count       = length(var.public_cidr)
+  subnet_id   = aws_subnet.public_subnet[count.index].id
+  route_table_id = aws_route_table.public_route_table[count.index].id
 }
 
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,5 +1,5 @@
 variable "region" {}
 variable "project" {}
 variable "vpc_cidr" {}
-variable "public_cidr" {}
+variable "public_cidr" {type = list(string)}
 variable "private_cidr" {}
